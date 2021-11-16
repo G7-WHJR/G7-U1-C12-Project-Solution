0f2852658e50268774ce4e171ca89609e18f9151
unknown <raveen.anand@whitehatjr.com>
2021-11-16 19:19:17 +0530

    v2

--- Traffic Signal/sketch.js
+++ Traffic Signal/sketch.js
@@ -1,16 +1,11 @@
 var yellow,red,green;
-var rectangle;
+
 function setup() {
-  //size of the canvas to draw on
   createCanvas(innerWidth, innerHeight);
-
-  // Set the background to white, and move the line to setup();
   background("white");
-
   fill("black");
   rect(100, 50, 100, 300); 
   rect(140, 350, 20, 300);
-
 }
 
 
